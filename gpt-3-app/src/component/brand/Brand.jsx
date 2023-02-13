7fbf9b3ef667d124d6d7b00367eb4bbf06124f3b
Abdullahi Aliyu <abdullahiemoabino2@gmail.com>
2023-02-13 22:58:16 +0100

    gpt-3 rendering: possibility section

--- gpt-3-app/src/component/brand/Brand.jsx
+++ gpt-3-app/src/component/brand/Brand.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { atlassian, dropbox, google, shopify, slack,  } from './import'
+import './Brand.css'
 
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='gpt3__brand section__padding'>
+    <div >
+      <img src={google} alt='google' />
+    </div>
+    <div >
+      <img src={slack} alt='slack' />
+    </div>
+    <div >
+      <img src={atlassian} alt='atlassian' />
+    </div>
+    <div >
+      <img src={dropbox} alt='dropbox' />
+    </div>
+    <div >
+      <img src={shopify} alt='shopify' />
+    </div>
+    </div>
   )
 }
 
--- gpt-3-app/src/component/navbar/NAvbar.jsx
+++ gpt-3-app/src/component/navbar/NAvbar.jsx
@@ -1,14 +1,71 @@
-import React from "react";
-import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
+import React, { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import logo from "../../assets/logo.svg";
 import "./NAvbar.css";
 
+const Menu = () => (
+  <>
+    <p>
+      <a href="#home">Home</a>
+    </p>
+    <p>
+      <a href="#wgpt3">what is GPT?</a>
+    </p>
+    <p>
+      <a href="#possibility">Open AI</a>
+    </p>
+    <p>
+      <a href="#features">Case Studies</a>
+    </p>
+    <p>
+      <a href="#blog">library</a>
+    </p>
+  </>
+);
+
 const NAvbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links_logo">
           <img src={logo} alt="" />
+        </div>
+        <div className="gpt3__navbar-links_container">
+          <Menu />
+        </div>
+        <div className="gpt3__navbar-sign">
+          <p>Sign In</p>
+          <button type="button">Sign Up</button>
+        </div>
+
+        <div className="gpt3__navbar-menu">
+          {toggleMenu ? (
+            <RiCloseLine
+              color="#fff"
+              size={27}
+              onClick={() => setToggleMenu(false)}
+            />
+          ) : (
+            <RiMenu3Line
+              color="#fff"
+              size={27}
+              onClick={() => setToggleMenu(true)}
+            />
+          )}
+
+          {toggleMenu && (
+            <div className="gpt3__navbar-menu_container scale-up">
+              <div className="gpt3__navbar-menu_container-links">
+              <Menu />
+                <div className="gpt3__navbar-menu_container-links-sign">
+                  <p>Sign In</p>
+                  <button type="button">Sign Up</button>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
