8695a4350a75f46f50d683de4fc6c066e350d914
Abdullahi Aliyu <abdullahiemoabino2@gmail.com>
2023-02-15 08:12:32 +0100

    completted blog section

--- gpt-3-app/src/component/article/Article.jsx
+++ gpt-3-app/src/component/article/Article.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import './Article.css'
 
-const article = () => {
+const Article = ({imgUrl,date, title}) => {
   return (
-    <div>article</div>
+    <div className='gpt3__blog-container_article'>
+      <div className='gpt3__blog-container-image'>
+      <img  src={imgUrl} alt='blog'/>
+      </div>
+      <div className='gpt3__blog-container_article-content'>
+      <div>
+      <p>{date}</p>
+      <h3>{title}</h3>
+      </div>
+      <p>Read Full Article</p>
+      </div>
+    </div>
   )
 }
 
-export default article
+export default Article
--- gpt-3-app/src/container/footer/Footer.jsx
+++ gpt-3-app/src/container/footer/Footer.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className ='gpt3__footer section__padding'>
+    <div className='gpt3__footer-heading'>
+      <h1 className='gradient__text'>Do you want to step in to the future before others </h1>
+      <div className='gpt3__footer-btn'>
+        <button>Request Early Access</button>
+      </div>
+    </div>
+    <div></div>
+    </div>
   )
 }
 
