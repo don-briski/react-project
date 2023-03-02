d3375714181ef64494d050c0611e67927cd94e26
foyedun <foyedun@gmail.com>
2023-03-02 20:41:25 +0100

    react with taiwind css

---
+++ bank-app/src/components/index.js
@@ -0,0 +1,16 @@
+import Billing from './Billing'
+import CTA from './CTA'
+import Footer from './Footer'
+import CardDeals from './CardDeals'
+import Business from './Business'
+import  Clients  from './Clients'
+import Testimonials from './Testimonials'
+import Hero from './Hero'
+import Navbar from './Navbar'
+import FeedbackCard from './FeedbackCard.jsx'
+import Button from './Button'
+import Getstarted from './Getstarted'
+import Stats from './Stats'
+
+
+export {Clients, CTA, Footer, CardDeals, Testimonials, Hero, Navbar, Billing, Business, FeedbackCard, Button, Getstarted, Stats}
